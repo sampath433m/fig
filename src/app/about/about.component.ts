77ce74125dba98c233624f39c8dbc9ba2182243b
sampath <sampathreddy433@gmail.com>
2017-09-04 21:49:21 -0600

    task11

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent  {
 
    image= [
-    {  'url': './assets/images/image1.JPG'},
+    {  'url': './assets/images/image1.jpg'},
     {  'url': './assets/images/image2.jpg'},
-    {  'url': './assets/images/image3.PNG'},
-    {  'url': './assets/images/image4.png'},
-    {  'url': './assets/images/image5.JPEG'}
+    {  'url': './assets/images/image3.jpg'},
+    {  'url': './assets/images/image4.jpg'},
+    {  'url': './assets/images/image5.jpg'}
 
   ];
 
