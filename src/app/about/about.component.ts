b957254c9a5343cbe2dcf81bb7b0c48b9622965a
sampath <sampathreddy433@gmail.com>
2017-09-04 21:45:20 -0600

    task10

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,10 +9,10 @@ export class AboutComponent  {
 
    image= [
     {  'url': './assets/images/image1.JPG'},
-    {  'url': './assets/images/image2.JPG'},
-    {  'url': './assets/images/image3.JPG'},
-    {  'url': './assets/images/image4.JPG'},
-    {  'url': './assets/images/image5.JPG'}
+    {  'url': './assets/images/image2.jpg'},
+    {  'url': './assets/images/image3.PNG'},
+    {  'url': './assets/images/image4.png'},
+    {  'url': './assets/images/image5.JPEG'}
 
   ];
 
